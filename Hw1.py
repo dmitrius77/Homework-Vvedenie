e427aca741c343328ff6a70b677f2305e14e9a0e
dmitrius77 <77dmitrius7777@gmail.com>
2024-10-31 20:09:11 +0500

    Add files via upload

---
+++ Hw1.py
@@ -0,0 +1,12 @@
+print(12+89-1)
+#1st program
+print(9**0.5*5)
+#2st program
+print(99.99>99.98 and 1000!=1000.1)
+#3st program
+print(2*2+2)
+print(2*(2+2))
+print(6==8)
+#4st program
+num = float('123.456') * 10
+print(int(num) % 10)
